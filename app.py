f5b7b3ebef7e19e208573050a34371f6493188d3
shreyas <97510965+codeeater800@users.noreply.github.com>
2024-10-13 14:59:09 +0530

    misc

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, request, jsonify
 from indic_transliteration import sanscript
 
 app = Flask(__name__)
@@ -12,10 +12,9 @@ def transliterate():
     data = request.get_json()
     text_to_transliterate = data['text']
 
-    # Perform transliteration (IAST to Devanagari example)
     transliterated_text = sanscript.transliterate(text_to_transliterate, sanscript.IAST, sanscript.DEVANAGARI)
 
     return jsonify({'transliteratedText': transliterated_text})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
