960e2a1561c28491005621a2c32bc369f558f584
shreyas <97510965+codeeater800@users.noreply.github.com>
2024-10-13 14:24:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -7,4 +8,6 @@ def test_api():
     return jsonify({"message": "Hello from the backend!"})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    # Get the port from the environment variable and use it, otherwise default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
