2188699db1e0f733cdfcc8eacafd348c04a3fed1
shreyas <97510965+codeeater800@users.noreply.github.com>
2024-10-13 14:40:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -7,7 +6,4 @@ app = Flask(__name__)
 def test_api():
     return jsonify({"message": "Hello from the backend!"})
 
-if __name__ == "__main__":
-    # Get the port from the environment variable and use it, otherwise default to 5000
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+# No need to include __main__ as Gunicorn will handle the app initialization
